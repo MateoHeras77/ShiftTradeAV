e7f6bfc30047b134e8f06b8615afe62be579334c
Mateo Heras <66641868+MateoHeras77@users.noreply.github.com>
2025-06-11 18:04:17 -0400

    Update 1_Formulario.py

--- 1_Formulario.py
+++ 1_Formulario.py
@@ -65,7 +65,7 @@ flight_options = [
     "AV205 (20:00-00:30+1)",  # Overnight flight - arrives next day
     "AV625 (20:00-02:30+1)",  # Overnight flight - arrives next day
     "AV255-AV627 Full Day (5:00-17:30)",
-    "AV619-AV627 Full Day (5:00-17:30)",
+    "AV619-AV627 Full Day (4:00-17:30)",
     "AV627-AV205 Full Day (13:00-00:30+1)",  # Overnight flight - arrives next day
 ]
 
